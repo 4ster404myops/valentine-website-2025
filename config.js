bf77cf9be1ef99d2f1b5ae46c0fcc174829e1fe7
4ster404myops <deathgun0128@gmail.com>
2025-02-12 22:19:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ const CONFIG = {
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
-            startText: "Awww, just this much? :((",                                   // Text before the percentage
+            startText: "♡⸜(˶˃ ᵕ ˂˶)⸝♡",                                   // Text before the percentage
             nextBtn: "Here!❤️"                                         // Text for the next button
         },
         third: {
